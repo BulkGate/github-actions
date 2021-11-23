28b8e3de016cbcaf6a3ff98f3cc78537df9aee67
MarekPijak <marekpijak@gmail.com>
2021-11-23 16:06:20 +0100

    new build

--- dist/index.js
+++ dist/index.js
@@ -8447,13 +8447,39 @@ const github = __nccwpck_require__(2357);
 
 try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    const url = core.getInput('url');
+    //const method = core.getInput('method');
+    //const contentType = core.getInput('contentType');
+    //const application_id = core.getInput('data');
+
+
+    console.log(`URL ${url}!`);
+    console.log("here");
+    //console.log(`Hello ${application_token}!`);
+    //console.log(`Hello ${contentType}!`);
+
+    /*var xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.setRequestHeader('Content-Type', contentType);
+    xhr.send(JSON.stringify({
+        application_id: "1234",
+        application_token: "test",
+        number: "420777777777"
+    }));
+    xhr.onload = function() {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        core.setOutput("response", data);
+    };*/
+
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+
+
+
+    //const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //console.log(`The event payload: ${payload}`);
+
+
 } catch (error) {
     core.setFailed(error.message);
 }
