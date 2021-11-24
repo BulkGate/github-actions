81e61da66f2259ecd2be0aafe5883de37e849151
MarekPijak <marekpijak@gmail.com>
2021-11-24 13:22:34 +0100

    test

--- dist/index.js
+++ dist/index.js
@@ -8444,7 +8444,7 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(199);
 const github = __nccwpck_require__(2357);
-const http = __nccwpck_require__(5687);
+const https = __nccwpck_require__(5687);
 
 try {
     const url = core.getInput('url');
@@ -8456,8 +8456,8 @@ try {
     console.log(`URL ${url}!`);
 
     const options = {
-        hostname: 'https://portal.bulkgate.com/api/1.0/simple/transactional',
-        path: 'https://portal.bulkgate.com/api/1.0/simple/transactional',
+        hostname: 'https://portal.bulkgate.com',
+        path: '/api/1.0/simple/transactional',
         method: 'POST'
     };
 
