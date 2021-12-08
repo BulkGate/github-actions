d0359dbb2443477c5cdf6652828d9e46deaaaf0a
MarekPijak <marekpijak@gmail.com>
2021-12-08 13:11:54 +0100

    report workflow

--- dist/index.js
+++ dist/index.js
@@ -8471,11 +8471,11 @@ try {
         channel: core.getInput('channel')
     }
 
-    //parameters.channel = JSON.parse(parameters.channel);
+    parameters.channel = JSON.parse(parameters.channel);
 
     const post_data = querystring.stringify(parameters);
 
-    console.log(JSON.parse(parameters));
+    console.log(parameters);
     console.log('------');
     console.log(post_data);
 
